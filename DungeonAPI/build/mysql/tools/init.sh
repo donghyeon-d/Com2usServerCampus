d0bc477f0c5e937734d11937a7d17950603c83fd
Dongheyon <ehdgud6634@gmail.com>
2023-05-23 02:19:18 +0900

    wip : docker compose mariadb

--- DungeonAPI/build/mysql/tools/init.sh
+++ DungeonAPI/build/mysql/tools/init.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
 echo "CREATE USER 'com2us'@'%' IDENTIFIED BY '123123';
 GRANT ALL PRIVILEGES ON *.* TO 'com2us'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;" > /tools/query/mysql_init.sql  && 
-
-service mariadb start && 
-mariadb < /tools/query/mysql_init.sql && 
-mariadb < /tools/query/mysql_account.sql && 
-mariadb < /tools/query/mysql_master.sql && 
-mariadb < /tools/query/mysql_master_data.sql && 
-mariadb < /tools/query/mysql_game.sql && 
-service mariadb stop  && 
-
-exec mysqld --bind-address=0.0.0.0
+FLUSH PRIVILEGES;" > mysql_init.sql;\
+chmod 777 ./mysql_init.sql;\
+service mysql start && \
+mysql < mysql_init.sql;\
+mysql < mysql_account.sql;\
+mysql < mysql_master.sql;\
+mysql < mysql_master_data.sql;\
+mysql < mysql_game.sql;\
+service mysql stop 
+# exec mysqld --bind-address=0.0.0.0
